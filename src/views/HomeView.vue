4e380c6aa66fcf73d78d02bf4373882bc6108471
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-07-01 17:00:53 -0300

    Create a project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main-container">
+    <div class="container-form">
+      <div class="container-title">
+        <h1 class="title">Cadastre-se</h1>
+      </div>
+      <form action=""></form>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.main-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  color: #41b2d8;
+}
+</style>
